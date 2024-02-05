cfff61c8d76cb09e8e048025cc54d6214ab64a8c
Tshikovha28 <vhenganiem28@gmail.com>
2024-02-04 17:08:06 -0800

    mod.c

---
+++ mod.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * f_mod - computes the rest of the division of the second
+ * top element of the stack by the top element of the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_mod(stack_t **head, unsigned int counter)
+{
+        stack_t *current;
+        int stack_size = 0, result;
+
+        current = *head;
+        while (current)
+        {
+                current = current->next;
+                stack_size++;
+        }
+        if (stack_size < 2)
+        {
+                fprintf(stderr, "L%d: not enough elements to perform mod operation\n", counter);
+                fclose(file_pointer);
+                free(file_content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        current = *head;
+        if (current->n == 0)
+        {
+                fprintf(stderr, "L%d: division by zero is not allowed\n", counter);
+                fclose(file_pointer);
+                free(file_content);
+                free_stack(*head);
+                exit(EXIT_FAILURE);
+        }
+        result = current->next->n % current->n;
+        current->next->n = result;
+        *head = current->next;
+        free(current);
+}
